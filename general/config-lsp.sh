a9e285e9cc42eb4ef6b8dde61942673ce2f4f3ac
Myungjin Lee <myungjin.lee@gmail.com>
2020-12-27 11:40:47 -0800

    emacs lsp config script update
    add os-specific behavior for installing lsp servers in different
    OSes

--- general/config-lsp.sh
+++ general/config-lsp.sh
@@ -1,3 +1,28 @@
 #!/usr/bin/env bash
 
-pip install python-language-server[all]
+function config_linux {
+    echo "Do nothing"
+}
+
+function config_macos {
+    brew install ccls
+}
+
+function config_common {
+     pip install python-language-server[all]
+}
+
+function main {
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        config_linux
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        config_macos
+    else
+	echo "$OSTYPE not supported"
+	exit 1
+    fi
+
+    config_common
+}
+
+main
