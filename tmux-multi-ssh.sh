8aeeb5633457b3b12f5eb8a90ed186bf11b1f37b
Myungjin Lee <myungjle@cisco.com>
2023-09-14 13:50:10 -0700

    fix: minor fix

--- tmux-multi-ssh.sh
+++ tmux-multi-ssh.sh
@@ -27,7 +27,7 @@ starttmux() {
     local hosts=( $HOSTS )
 
     # tmux new-window "ssh ${hosts[0]}"
-    # unset hosts[0];
+    unset hosts[0];
     for host in "${hosts[@]}"; do
         for i in $(seq 1 $n_copies); do
             tmux split-window -h "ssh $host"
