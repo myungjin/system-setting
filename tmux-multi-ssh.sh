15bfbb76b245d91db73e1bdc2606db74015584b7
Myungjin Lee <myungjle@cisco.com>
2023-09-14 12:47:43 -0700

    revise tmux multi ssh

--- tmux-multi-ssh.sh
+++ tmux-multi-ssh.sh
@@ -2,6 +2,22 @@
 
 # run tmux before executing this script
 
+n_copies=1
+
+while getopts 'n:h' opt; do
+    case "$opt" in
+        n) n_copies=$(($OPTARG))
+           echo "$n_copies"
+           ;;
+   
+        ?|h)
+            echo "Usage: $(basename $0) [-n <num>] <a list of hosts>"
+            exit 1
+            ;;
+    esac
+done
+shift "$(($OPTIND -1))"
+
 starttmux() {
     if [ -z "$HOSTS" ]; then
        echo -n "Please provide of list of hosts separated by spaces [ENTER]: "
@@ -10,11 +26,13 @@ starttmux() {
 
     local hosts=( $HOSTS )
 
-    tmux new-window "ssh ${hosts[0]}"
-    unset hosts[0];
-    for i in "${hosts[@]}"; do
-        tmux split-window -h  "ssh $i"
-        tmux select-layout tiled > /dev/null
+    # tmux new-window "ssh ${hosts[0]}"
+    # unset hosts[0];
+    for host in "${hosts[@]}"; do
+        for i in $(seq 1 $n_copies); do
+            tmux split-window -h "ssh $host"
+            tmux select-layout tiled > /dev/null
+        done
     done
     tmux select-pane -t 0
     tmux set-window-option synchronize-panes on > /dev/null
