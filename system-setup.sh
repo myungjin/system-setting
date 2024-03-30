922fa9db4ef2b8280444cb1113a817f32c698070
Myungjin Lee <myungjin.lee@gmail.com>
2024-03-30 15:02:33 -0700

    add ubuntu config

--- system-setup.sh
+++ system-setup.sh
@@ -1,6 +1,35 @@
 #!/usr/bin/env bash
 
-function config_linux {
+function config_ubuntu {
+    sudo apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
+         libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
+         libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python3-openssl git
+
+    curl https://pyenv.run | bash
+
+    if grep "PYENV_ROOT" ~/.bashrc; then
+        echo "pyenv is already configured"
+    else
+        echo >> ~/.bashrc
+        echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+        echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+        echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n eval "$(pyenv init -)"\nfi' >> ~/.bashrc
+    fi
+
+    pyenv install 3.10.14
+    pyenv global 3.10.14
+
+    # install python packages
+    pip install python-lsp-server[all]
+    pip install pyls-black pyls-isort pyls-mypy
+    pip install future
+    pip install autopep8
+    pip install isort
+    pip install ruamel.yaml
+    pip install numpy
+}
+
+function config_archlinux {
     sudo pacman -S ttf-fira-code cantarell-fonts ccls python-pip
 
     # install golang language server
@@ -65,7 +94,15 @@ function config_common {
 
 function main {
     if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-        config_linux
+        distribution=$(lsb_release -i)
+        if [[ $distribution == *"Ubuntu"* ]]; then
+            config_ubuntu
+        elif [[ $distribution == *"archlinux"* ]]; then
+            config_archlinux
+        else
+            echo "$distribution not supported"
+            exit 1
+        fi
     elif [[ "$OSTYPE" == "darwin"* ]]; then
         config_macos
     else
